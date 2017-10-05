bc6bb86b0b619fd14b32a818f4a262b76f705876
Filip Branchetti <branchetti@hotmail.se>
2017-10-05 13:52:00 +0200

    added styled-component to stars

--- src/stars/stars.js
+++ src/stars/stars.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
-import './stars.css';
+import styled from 'styled-components';
 
+const StarBlock = styled.div``;
+const StarBlockTitle = styled.h2`
+  margin-bottom: 0;
+`;
+const StarsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
 
+const Star = styled.span`
+  padding: 20px 20px;
+  cursor: pointer;
+  width:50px;
+  height:50px;
+  overflow:hidden;
+  position:relative;
+  background:url('data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB3aWR0aD0iMjBweCIgaGVpZ2h0PSIyMHB4IiB2aWV3Qm94PSIwIDAgMjAgMjAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDIwIDIwIiB4bWw6c3BhY2U9InByZXNlcnZlIj48cG9seWdvbiBmaWxsPSIjREREREREIiBwb2ludHM9IjEwLDAgMTMuMDksNi41ODMgMjAsNy42MzkgMTUsMTIuNzY0IDE2LjE4LDIwIDEwLDE2LjU4MyAzLjgyLDIwIDUsMTIuNzY0IDAsNy42MzkgNi45MSw2LjU4MyAiLz48L3N2Zz4=');
+  background-size: contain;
+  background-repeat:no-repeat;
+
+  &:hover,&.active {
+    color: red;
+    background:
+      url('data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB3aWR0aD0iMjBweCIgaGVpZ2h0PSIyMHB4IiB2aWV3Qm94PSIwIDAgMjAgMjAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDIwIDIwIiB4bWw6c3BhY2U9InByZXNlcnZlIj48cG9seWdvbiBmaWxsPSIjRkZERjg4IiBwb2ludHM9IjEwLDAgMTMuMDksNi41ODMgMjAsNy42MzkgMTUsMTIuNzY0IDE2LjE4LDIwIDEwLDE2LjU4MyAzLjgyLDIwIDUsMTIuNzY0IDAsNy42MzkgNi45MSw2LjU4MyAiLz48L3N2Zz4=');
+      background-size: contain;
+      background-repeat:no-repeat;
+  }
+
+`;
 
 class Stars extends Component {
   constructor(props) {
@@ -32,23 +61,23 @@ class Stars extends Component {
     let rows = [], i = 0, len = this.props.stars;
     while (++i <= len){rows.push(i);}
     return (
-      <div className="star-block" >
-        <h2 className="star-block__title">{this.props.title}</h2>
-        <div className="star-block__stars">
+      <StarBlock>
+        <StarBlockTitle>{this.props.title}</StarBlockTitle>
+        <StarsContainer>
         {rows.map(starNumber => {
           return(
-             <span
-               className={this.activeNumber >= starNumber ? 'star-block__stars__star star-block__stars__star--active' : 'star-block__stars__star'}
+             <Star
+               className={this.activeNumber >= starNumber ? 'active' : ''}
                key={starNumber}
                data-id={starNumber}
                onClick={this.handleClick.bind(this)}
                onMouseEnter={this.hoverStarEnter.bind(this)}
                onMouseLeave={this.hoverStarLeave.bind(this)}>
-             </span>
+             </Star>
           )}
         )}
-        </div>
-      </div>
+        </StarsContainer>
+      </StarBlock>
     );
   }
 }
